4eb956a537fdfc5d4297b87ad927855bb9fe9cce
Child of the Beast <dragon_5556@msn.com>
2020-09-17 01:59:24 -0400

    Update WorldJoin.cs

--- Basics/PlayerJoinBell/WorldJoin.cs
+++ Basics/PlayerJoinBell/WorldJoin.cs
@@ -8,9 +8,11 @@ public class WorldJoin : UdonSharpBehaviour
     public AudioSource Bell;
     public AudioClip Join;
     public AudioClip Leave;
+    private bool JoinEnable = true;
+    private bool LeaveEnable = true;
     public override void OnPlayerJoined(VRCPlayerApi player)
     {
-        if (Join != null)
+        if (Join != null && JoinEnable)
         {
             Bell.clip = Join;
             Bell.Play();
@@ -18,10 +20,19 @@ public class WorldJoin : UdonSharpBehaviour
     }
     public override void OnPlayerLeft(VRCPlayerApi player)
     {
-        if (Leave != null)
+        if (Leave != null && LeaveEnable)
         {
             Bell.clip = Leave;
             Bell.Play();
         }
+    }
+
+    public void JoinToggle()
+    {
+        JoinEnable = !JoinEnable;
+    }
+    public void LeaveToggle()
+    {
+        LeaveEnable = !LeaveEnable;
     }
 }
