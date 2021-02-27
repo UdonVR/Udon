e32de475ed51401d1ee0e23c6da5e3152bed1240
MrsJessica92 <2132554+MrsJessica92@users.noreply.github.com>
2021-02-27 18:57:25 +0100

    forgot a small fix

--- Games/Beerpong/BeerPong.cs
+++ Games/Beerpong/BeerPong.cs
@@ -16,7 +16,7 @@ public class BeerPong : UdonSharpBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.parent == ParentOfCups.transform) 
+        if (other.transform.parent == ParentOfCups.transform)
         {
             other.gameObject.SetActive(false);
             // Ball should respawn when in cup
@@ -26,7 +26,7 @@ public class BeerPong : UdonSharpBehaviour
 
     public GameObject BallSpawn;
     private Vector3 Ball_Spawn;
-    
+
     private float maxDistance = 6.5f;
     private float lastMove = 0;
 
@@ -34,7 +34,7 @@ public class BeerPong : UdonSharpBehaviour
     {
         Ball_Spawn = BallSpawn.transform.position;
     }
-    
+
     /// <summary>
     /// Automatic Ball Respawn on:
     /// - > 2 Seconds no Movement but also not in hand of a player
@@ -45,7 +45,7 @@ public class BeerPong : UdonSharpBehaviour
     {
         float currentDistance = (BallSpawn.transform.position - gameObject.transform.position).magnitude;
         float currentVelocity = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
-        VRC_Pickup pickup = (VRC_Pickup) gameObject.GetComponent(typeof(VRC_Pickup));
+        VRC_Pickup pickup = (VRC_Pickup)gameObject.GetComponent(typeof(VRC_Pickup));
 
         if (currentDistance > 0.2 && currentVelocity == 0 && !pickup.IsHeld)
         {
@@ -60,7 +60,7 @@ public class BeerPong : UdonSharpBehaviour
         }
 
         if (
-            currentDistance > maxDistance || 
+            currentDistance > maxDistance ||
             gameObject.transform.localPosition.y < -12
         )
         {
@@ -70,7 +70,7 @@ public class BeerPong : UdonSharpBehaviour
 
     public void RespawnBall()
     {
-        Networking.SetOwner(Networking.LocalPlayer, Ball);
+        Networking.SetOwner(Networking.LocalPlayer, gameObject);
         gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
         gameObject.transform.position = Ball_Spawn;
     }
