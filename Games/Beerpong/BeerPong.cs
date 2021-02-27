faf1f731d736e9be18f2856d174ab5f3bc2f91c8
MrsJessica92 <2132554+MrsJessica92@users.noreply.github.com>
2021-02-27 18:56:53 +0100

    Add Autorespawn of the Ball
    - > 2 Seconds no Movement but also not in hand of a player
    - Ball is under the table
    - Ball has a distance larger then necessary
    - On enter the cup

--- Games/Beerpong/BeerPong.cs
+++ Games/Beerpong/BeerPong.cs
@@ -13,29 +13,66 @@ using System.Collections;
 public class BeerPong : UdonSharpBehaviour
 {
     public GameObject ParentOfCups;
-    public GameObject Ball;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.parent == ParentOfCups.transform) 
         {
             other.gameObject.SetActive(false);
+            // Ball should respawn when in cup
+            RespawnBall();
         }
     }
 
     public GameObject BallSpawn;
     private Vector3 Ball_Spawn;
+    
+    private float maxDistance = 6.5f;
+    private float lastMove = 0;
 
     private void Start()
     {
         Ball_Spawn = BallSpawn.transform.position;
     }
+    
+    /// <summary>
+    /// Automatic Ball Respawn on:
+    /// - > 2 Seconds no Movement but also not in hand of a player
+    /// - Ball is under the table
+    /// - Ball has a distance larger then necessary
+    /// </summary>
+    private void Update()
+    {
+        float currentDistance = (BallSpawn.transform.position - gameObject.transform.position).magnitude;
+        float currentVelocity = gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+        VRC_Pickup pickup = (VRC_Pickup) gameObject.GetComponent(typeof(VRC_Pickup));
+
+        if (currentDistance > 0.2 && currentVelocity == 0 && !pickup.IsHeld)
+        {
+            if (Time.time - lastMove > 2)
+            {
+                RespawnBall();
+            }
+        }
+        else
+        {
+            lastMove = Time.time;
+        }
+
+        if (
+            currentDistance > maxDistance || 
+            gameObject.transform.localPosition.y < -12
+        )
+        {
+            RespawnBall();
+        }
+    }
 
     public void RespawnBall()
     {
         Networking.SetOwner(Networking.LocalPlayer, Ball);
-        Ball.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        Ball.transform.position = Ball_Spawn;
+        gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        gameObject.transform.position = Ball_Spawn;
     }
     public void Network_RespawnCups()
     {
